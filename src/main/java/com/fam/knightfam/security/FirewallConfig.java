60cdf87d60dcb9cb4150f4ec21351bd4a2d37f40
adam-knight1 <adam.knight@mykenzie.snhu.edu>
2025-03-29 16:03:54 -0400

    added comments to firewallconfig, only exisiting to allow semicolons at the moment

--- src/main/java/com/fam/knightfam/security/FirewallConfig.java
+++ src/main/java/com/fam/knightfam/security/FirewallConfig.java
@@ -5,15 +5,17 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.web.firewall.HttpFirewall;
 import org.springframework.security.web.firewall.StrictHttpFirewall;
 
-/* Using this to allow semicolons to stop getting malicious string error and not have to use cookies for sessions" */
+/* Using this firewall config class to allow semicolons to stop getting malicious string error and not have to use cookies for sessions" */
 
 @Configuration
 public class FirewallConfig {
 
+    /*THe main function of this class comes from this method, which only exists to allow semicolons to step failing tests*/
+
     @Bean
     public HttpFirewall allowSemicolonHttpFirewall() {
         StrictHttpFirewall firewall = new StrictHttpFirewall();
-        // Allowing semicolons in the URL
+        // Allowing semicolons in the URL for now to pass tests
         firewall.setAllowSemicolon(true);
         return firewall;
     }
