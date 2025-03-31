3eae836237197c3019b33d283c5795fdecf0d02e
adam-knight1 <adam.knight@mykenzie.snhu.edu>
2025-03-31 05:44:17 -0400

    troubleshooting docker container exiting upon start on ec2

--- src/main/java/com/fam/knightfam/controller/LoginController.java
+++ src/main/java/com/fam/knightfam/controller/LoginController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 /*basic login controller for temporary dev use, login maintained by cognito*/
-/* will expand when react frontend implemeneted */
+/* will expand when react frontend implemented */
 @Controller
 public class LoginController {
 
