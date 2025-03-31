25479d4bbdd18003b78c2862dadaba625f92a1f2
adam-knight1 <adam.knight@mykenzie.snhu.edu>
2025-03-31 05:06:35 -0400

    modified start container script

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,3 +1,8 @@
+
 #!/bin/bash
+# Authenticate Docker to ECR
+aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 913524908137.dkr.ecr.us-east-2.amazonaws.com
+
+# Pull and run the container
 docker pull 913524908137.dkr.ecr.us-east-2.amazonaws.com/backend-app:latest
 docker run -d --name knightfam -p 8080:8080 913524908137.dkr.ecr.us-east-2.amazonaws.com/backend-app:latest
