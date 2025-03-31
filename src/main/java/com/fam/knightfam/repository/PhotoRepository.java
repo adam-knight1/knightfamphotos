1999a7f478e86eaf1e6a17d3e83c374e6a0824ce
adam-knight1 <adam.knight@mykenzie.snhu.edu>
2025-03-31 00:45:50 -0400

    Testing new CICD pipeline to ECR and EC2

--- src/main/java/com/fam/knightfam/repository/PhotoRepository.java
+++ src/main/java/com/fam/knightfam/repository/PhotoRepository.java
@@ -6,5 +6,4 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PhotoRepository extends JpaRepository<Photo, Long> {
-    // You can add custom query methods here if needed
 }
