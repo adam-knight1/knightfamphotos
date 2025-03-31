145dcf99099a5c1cc8d9e743f6a6bf18cf02c72f
adam-knight1 <adam.knight@mykenzie.snhu.edu>
2025-03-31 05:16:19 -0400

    docker container running on ec2, deployed from codebuild, last troubleshooting step

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -6,3 +6,4 @@ aws ecr get-login-password --region us-east-2 | docker login --username AWS --pa
 # Pull and run the container
 docker pull 913524908137.dkr.ecr.us-east-2.amazonaws.com/backend-app:latest
 docker run -d --name knightfam -p 8080:8080 913524908137.dkr.ecr.us-east-2.amazonaws.com/backend-app:latest
+
