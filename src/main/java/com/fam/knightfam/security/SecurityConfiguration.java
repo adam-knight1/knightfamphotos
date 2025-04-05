6717abe612475654ebdb9799b2f608ac8c5b1dc0
adam-knight1 <adam.knight@mykenzie.snhu.edu>
2025-04-05 14:25:45 -0400

    Removed thymeleaf login page so it redirects to cognito login

--- src/main/java/com/fam/knightfam/security/SecurityConfiguration.java
+++ src/main/java/com/fam/knightfam/security/SecurityConfiguration.java
@@ -44,7 +44,6 @@ public class SecurityConfiguration {
                 )
                 .requestCache(cache -> cache.requestCache(new NullRequestCache()))
                 .oauth2Login(oauth -> oauth
-                        .loginPage("/login")
                         .defaultSuccessUrl("/", true)
                 )
                 .logout(logout -> logout.logoutSuccessHandler(logoutHandler));
