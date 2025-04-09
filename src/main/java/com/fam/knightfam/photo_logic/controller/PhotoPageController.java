b91b61888ced40bcbe0404a591d841789afb9b07
adam-knight1 <adam.knight@mykenzie.snhu.edu>
2025-04-09 12:02:17 -0400

    fixed name convention of TL gallery html

--- src/main/java/com/fam/knightfam/photo_logic/controller/PhotoPageController.java
+++ src/main/java/com/fam/knightfam/photo_logic/controller/PhotoPageController.java
@@ -5,6 +5,7 @@ import com.fam.knightfam.photo_logic.service.PhotoService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
